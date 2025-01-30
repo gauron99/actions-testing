e0a4c386c34aba8f8c071f16d68ea5ca0f34a962
David Fridrich <fridrich.david19@gmail.com>
2025-01-30 12:07:32 +0100

    fix
    Signed-off-by: David Fridrich <fridrich.david19@gmail.com>

---
+++ go-scripts/ib.sh
@@ -0,0 +1,10 @@
+  local kubectl_version=1.32.0
+  local kind_version=0.26.0
+  local dapr_version=1.11.0
+  local helm_version=3.12.0
+  local stern_version=1.25.0
+  local kn_version=1.13.0
+  local jq_version=1.7.1
+
+	local kn_serving=1.16.0
+	local kn_eventing=1.16.0

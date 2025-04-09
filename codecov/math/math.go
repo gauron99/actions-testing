55d6a0ff54c2a10ca2e498add9b99c268918dca5
David Fridrich <fridrich.david19@gmail.com>
2025-04-09 12:24:57 +0200

    action + go files
    Signed-off-by: David Fridrich <fridrich.david19@gmail.com>

---
+++ codecov/math/math.go
@@ -0,0 +1,16 @@
+package math
+
+func Plus(a, b int) int {
+	return a + b
+}
+func Minus(a, b int) int {
+	return a - b
+}
+func Times(a, b int) int {
+	return a * b
+}
+func Divide(a, b int) int {
+	return a / b
+}
+
+
